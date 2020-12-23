fba44c20a3817a91fdd326ddd27439c85878286a
Roma Levsha <rm.levsha@gmail.com>
2020-12-23 16:07:50 +0300

    feat: add buttons to main

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const drop = document.querySelector('.drop');
+const buttonPlay = document.querySelector('.play');
+const buttonShowHowPlay = document.querySelector('.howToPlay');
 console.log(drop);
+
+function hideButtons() {
+    buttonPlay.style.display ='none';
+    buttonShowHowPlay.style.display ='none';
+}
 
 function showDrop() {
 drop.classList.remove('hidden');
@@ -13,6 +20,7 @@ function hideDrop () {
     drop.classList.add('hidden');
 } 
 setInterval(() => {
+    hideButtons();
     setTimeout(showDrop, 100);
     setTimeout(moveDropDown, 200);
     setTimeout(() => {
